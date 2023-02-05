71bc428f5b3a540ac36b1efc123df4b3026be97d
djamiou1 <ydjamiou216@gmail.com>
2023-02-05 03:55:06 +0100

    signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { ProfileComponent } from './components/profile/profile.component';
 import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
 import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent,
-    SignupComponent,
-    ProfileComponent,
     RequestResetComponent,
     ResponseResetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+   form!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: HttpClient
+  ) { }
 
   ngOnInit(): void {
+     this.form = this.fb.group({
+       name:['', Validators.required],
+       email:['', Validators.required],
+       password:['', Validators.required]
+     });
   }
+  login(): void{
+    const val = this.form.value;
+    console.log(val);
+    // if (val.email && val.password) {
+    //     this.authService.login(val.email, val.password)
+    //         .subscribe(
+    //             () => {
+    //                 console.log("User is logged in");
+                    
+    //             }
+    //         );
+     }
 
 }
